c64517b0194ad235da38d3520743297d9ef4d351
Andrii Kochmaruk <luckyanry@gmail.com>
2020-05-13 08:46:46 +0200

    Task-4 is ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import './js-hw-01/task-1.js';
 // import './js-hw-01/task-2.js';
 // import './js-hw-01/task-3.js';
-import './js-hw-01/task-4.js';
+// import './js-hw-01/task-4.js';
 import './js-hw-01/task-5.js';
 import './js-hw-01/task-6.js';
--- js-hw-01/task-4.js
+++ js-hw-01/task-4.js
@@ -19,18 +19,23 @@ const credits = 23580;
 const pricePerDroid = 3000;
 let message;
 
-const input = prompt(
+let input = prompt(
   'Укажите количество дроидов, которые Вы желаете преобрести?',
 );
 
 if (input === null) {
   message = 'Отменено пользователем!';
 } else if (input > 0) {
+  input = Number(input);
   const totalPrice = input * pricePerDroid;
   const result = credits - totalPrice;
 
   if (totalPrice > credits) {
     message = 'Недостаточно средств на счету!';
+  } else if (input === 1) {
+    message = `Вы купили ${input} дроид, на счету осталось ${result} кредитов.`;
+  } else if (input >= 2 && input <= 4) {
+    message = `Вы купили ${input} дроида, на счету осталось ${result} кредитов.`;
   } else {
     message = `Вы купили ${input} дроидов, на счету осталось ${result} кредитов.`;
   }
