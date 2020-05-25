abdd1b2d196f8aad56387304564f62befbf85f42
Andrii Kochmaruk <luckyanry@gmail.com>
2020-05-25 11:21:35 +0200

    Finished task-2 of js-hw-05

--- js-hw-05/task-2.js
+++ js-hw-05/task-2.js
@@ -7,6 +7,20 @@
    User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 */
 
+class User {
+  constructor({ name = 'Name', age = 0, followers = 0 }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo = function () {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+    );
+  };
+}
+
 const mango = new User({
   name: 'Mango',
   age: 2,
