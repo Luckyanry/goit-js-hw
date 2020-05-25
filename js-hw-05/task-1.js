1a112f5f903cd6ac78542687207a702c1657b7ff
Andrii Kochmaruk <luckyanry@gmail.com>
2020-05-25 11:23:52 +0200

    Corrected task-1 of js-hw-05

--- js-hw-05/task-1.js
+++ js-hw-05/task-1.js
@@ -16,6 +16,8 @@ Account.prototype.getInfo = function () {
   console.log(`Login: ${this.login}, Email: ${this.email}`);
 };
 
+console.log(Account.prototype.getInfo); // function
+
 const mango = new Account({
   login: 'Mangozedog',
   email: 'mango@dog.woof',
