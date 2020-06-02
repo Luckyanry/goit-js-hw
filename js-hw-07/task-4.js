7034ac54481d3db899d1e6462ea28b71331ecf9b
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-02 23:15:42 +0200

    Finished task-4 of js-hw-07

--- js-hw-07/task-4.js
+++ js-hw-07/task-4.js
@@ -4,3 +4,19 @@
       Создай функции increment и decrement для увеличения и уменьшения значения счетчика
       Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 */
+
+const counterValue = document.querySelector('#value');
+
+const buttonDec = document.querySelector('button[data-action="decrement"]');
+const buttonInc = document.querySelector('button[data-action="increment"]');
+
+function decrement() {
+  counterValue.textContent -= 1;
+}
+
+function increment() {
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+}
+
+buttonDec.addEventListener('click', decrement);
+buttonInc.addEventListener('click', increment);
