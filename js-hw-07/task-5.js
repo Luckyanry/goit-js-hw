25cf9f03b90a2c27e8b8c502060b1cca40de095e
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-02 23:56:54 +0200

    Finished task-5 of js-hw-07

--- js-hw-07/task-5.js
+++ js-hw-07/task-5.js
@@ -3,3 +3,15 @@
     подставляет его текущее значение в span#name-output. Если инпут пустой, 
     в спане должна отображаться строка 'незнакомец'.
 */
+
+const inputText = document.querySelector('#name-input');
+const spanText = document.querySelector('#name-output');
+
+function insertName(event) {
+  spanText.textContent =
+    event.target.value === '' ? defaultText : event.target.value;
+}
+
+const defaultText = 'незнакомец';
+
+inputText.addEventListener('input', insertName);
